91207c83456c4de580fa1b52be8d3789cadcc264
Umashankar kumar <Umashankarkumar9572@gmail.com>
2026-05-15 23:59:09 +0530

    Update GoalProgress.php

--- Backend/app/Events/GoalProgress.php
+++ Backend/app/Events/GoalProgress.php
@@ -3,10 +3,14 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
+
 use Illuminate\Broadcasting\PrivateChannel;
+
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+
 
 class GoalProgress implements ShouldBroadcast
 {
